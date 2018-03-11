0568895f16e2346585c5e039e9347181802a5e25
rajendranneil19 <rajendranneil19@gmail.com>
2018-03-10 23:53:10 -0500

    added user Schema /model and Medical condition schema and model

---
+++ db/MedicalCondition.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema 
+
+const MedicalConditionSchema = new Schema ({
+    name: String,
+    description: String,
+    symptoms: String, 
+    dateStarted: String,
+    treatment: []
+
+
+})
+
+// MODEL 
+const MedicalCondition = mongoose.mode;('MedicalCondition', MedicalConditionSchema)
+
+module.exports = {
+    MedicalCondition,
+    MedicalConditionSchema
+}
