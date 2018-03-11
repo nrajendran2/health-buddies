e1f16625162fb1501114c15ed2f3ae9888cb3dc3
rajendranneil19 <rajendranneil19@gmail.com>
2018-03-10 22:19:10 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Purpose: HealthBuddies is an app which promotes individuals to share their expierences with their health. By being transparent with their health, users will be able to see others who have similar health problems. This will then promoter a positive envoirment for people who have similar health issues.
+
+trello: https://trello.com/b/qpQnl88p/health-buddies
+
+
+ERD: https://i.imgur.com/KpDtFVQ.jpg 
+
+wireframes: https://i.imgur.com/xU1lpOk.jpg
+
---
+++ server.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+
+const app = express()
+
+
+//connect to mongoose
+mongoose.connect(process.env.MONGODB_URI)
+
+
+//Logging if Database if is running or if there is an error
+
+const connection = mongoose.connection
+connection.on('connected', () => {
+    console.log("Mongoose is rocking and rolling")
+})
+connection.on('error', (Error )=> {
+    console.log("Mongoose Error:", error)
+})
+
+//MIDDLEWARE
+app.use(logger('dev'))
+app.use(bodyParser.json)
+
+
+
+
+//routes
+app.get('/', (req, res) => {
+    res.send("Hell World")
+})
+
+
+
+const PORT = process.env.PORT || 3001
+
+
+app.listen(PORT, () => {
+    console.log('App is listening on port '  +  PORT)
+})
