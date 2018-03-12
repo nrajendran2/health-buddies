840576f14e0bf0fb9cb747c6ee630eb041f46eee
rajendranneil19 <rajendranneil19@gmail.com>
2018-03-12 17:11:29 -0400

    Got the create new for the medical condition

--- Controllers/medicalConditionController.js
+++ Controllers/medicalConditionController.js
@@ -1,20 +1,49 @@
 const express = require('express')
-const router = express.Router({mergeParams: true})
+const router = express.Router({ mergeParams: true })
 const { MedicalCondition } = require('../db/MedicalCondition.js')
-const {User} = require('../db/UserSchema')
+const { User } = require('../db/UserSchema')
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     User.findById(req.params.id).then((user) => {
         res.json(user.medicalCondition)
-    }).catch((error)=> {
+    }).catch((error) => {
         console.log(error)
     })
 
 })
 
+router.post('/', (req, res) => {
+    const userId = req.params.id
+    console.log(userId)
+    User.findById(userId).then((user) => {
+        console.log(user)
+
+        const newMedicalCondition = new MedicalCondition({
+            name: req.body.name,
+            symptoms: req.body.symptoms,
+            dateStarted: req.body.dateStarted
+        })
+
+        user.medicalCondition.push(newMedicalCondition)
+        
+        // Save User
+        return user.save()
+    }).then((updatedUser) => {
+
+        //redirect to medical conditions 
+        res.redirect(`/api/users/${userId}/medicalcondition`)
+
+    })
+})
 
 
-module.exports = router 
+
+
+
+
+
+
+module.exports = router
 
 
 
