64d18f30b7b8f00aaf26cebea0de6afddcd23ac1
rajendranneil19 <rajendranneil19@gmail.com>
2018-03-12 13:05:41 -0400

    DATABASE HAS BEEN SEEDED WITH ONE EXAMPLE

--- db/Treatment.js
+++ db/Treatment.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema 
 
 const TreatmentSchema = new Schema ({
-    symptoms: String,
-    dcotor: String,
+    name: String,
+    doctor: String,
     image: String,
     mediations: String,
     naturalRemedies: String,
--- db/seeds.js
+++ db/seeds.js
@@ -1,12 +1,61 @@
-require('dotenv').config
-const mongoose = require('mnogoose')
-const {User} = require('/UserSchema')
+require('dotenv').config()
+const mongoose = require('mongoose')
+const {User} = require('./UserSchema')
+const {MedicalCondition} = require('./MedicalCondition')
+const {Treatment} = require('./Treatment')
+
+console.log(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+
+db.once('open', ()=> {
+    console.log("Mongoose on Seeds has connected to the database")
+})
+
+db.on('error', (error) => {
+    console.error(`MongoDB connection error!!! ${error} `)
+  process.exit(-1)
+})
+
+
+
+const masectomy = new Treatment ({
+    name: "masectomy",
+    doctor: "Dr. McQuail",
+    image: "https://www.mayoclinic.org/-/media/kcms/gbs/patient-consumer/images/2017/09/29/16/35/bc-3651003-001-0-8col.jpg",
+    medications: "Ibprofuen, Hydrochloride",
+    naturalRemedies:"Exercise, Drawing, Painting with my Daughters",
+    OtherComments: "At first I was really scared of losing one of my breast, but Dr. McQuail walked me thorugh a plan and after 3 months it's been going well."
+    
+})
+
+
+const breastcancer =  new MedicalCondition ({
+    name: "Breast Cancer",
+    symptoms: "Lump in chest, pain in chest",
+    dateStarted: "3/04/18",
+    treatment: [masectomy]
+})
 
 
 const sophia = new User ({
     name: "Sophia Guilliod",
     username:"SophiaGuilliod24",
     age: 50,
-    //medicalCondition: []
+    medicalCondition: [breastcancer]
     
+})
+
+
+
+User.remove()
+.then(()=> {
+        return User.remove()
+}).then(()=> {
+    return User.insertMany([sophia])
+}).then(()=> {
+    console.log("saved users")
+    db.close()
+}).catch((err)=> {
+    console.log(err)
 })
